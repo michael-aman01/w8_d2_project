82817f69de3b56c47688e422f2497089a07b85a9
michael <michael_aman92@icloud.com>
2022-09-27 14:40:57 -0700

    add phase 2

--- skeleton/phase_1_arrays.js
+++ skeleton/phase_1_arrays.js
@@ -1,3 +1,37 @@
-let arr = new Array
-arr = [1,2,3]
-console.log(arr)
+Array.prototype.uniq = function(){
+    let newArray = [];
+    for(let ele of this){
+        if (newArray.includes(ele) === false){
+            newArray.push(ele);
+        }
+    }
+    return newArray;
+}
+
+
+Array.prototype.twoSum = function(){
+    let sums = [];
+    for(let i = 0; i < this.length; i++){
+        for(let j = i + 1; j < this.length; j++){
+            if(this[i] + this[j] === 0){
+                sums.push([i,j]);
+            }
+        }
+    }
+    return sums; 
+}
+
+
+Array.prototype.transpose = function(){
+    let transposed = [];
+    for(let i = 0; i < this[0].length; i++){
+        let row = [];
+        for(let j = 0; j < this.length; j++){
+                row.push(this[j][i]);
+        }
+        transposed.push(row);
+    }
+    return transposed;
+}
+
+
